d9119bc541f3f313cc422300902e517ec1317234
Cem Onyuksel <cem.onyuksel@cfa.harvard.edu>
2024-10-14 17:26:34 -0400

    define config_columns

--- core/models/config_columns.py
+++ core/models/config_columns.py
@@ -1,4 +1,8 @@
 CONFIG_COL = [
-    'number',
-    'line'        
+    "order_idx", # order index
+    "optical_order", # optical order
+    "object", # target object name
+    "source", # sky/sci/cal
+    "chip_idx", # detector chip index
+    "trace_idx", # trace (slice, etc.) index
 ]
