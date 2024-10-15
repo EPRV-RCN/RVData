2680de7e4cb7ab90ce06df9abe0ff563fea6c4c7
T. E. Pickering <te.pickering@gmail.com>
2024-10-14 23:40:18 -0400

    codestyle cleanup using black plus a few other fixes it missed

--- core/models/receipt_columns.py
+++ core/models/receipt_columns.py
@@ -1,8 +1,8 @@
 RECEIPT_COL = [
-    'Time',         
-    'Code_Release', 
-    'Branch_Name',  
-    'Commit_Hash',  
-    'Module_Name',  
-    'Status'
+    "Time",
+    "Code_Release",
+    "Branch_Name",
+    "Commit_Hash",
+    "Module_Name",
+    "Status",
 ]
