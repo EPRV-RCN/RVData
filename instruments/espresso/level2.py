0a75bf577cc1c027b5d38259ad7060b146cc8acc
loris.jacques@yahoo.fr <loris.jacques@yahoo.fr>
2025-03-06 09:00:09 +0100

    New version espresso in the same format as HARPS and HARPN

--- instruments/espresso/level2.py
+++ instruments/espresso/level2.py
@@ -1,24 +1,39 @@
-import os
-from collections import OrderedDict
-import importlib
+'''
+RVData/instruments/espresso/level2.py
+
+UNIGE-ESO - EPRV
+Author: Loris JACQUES
+Created: Mon Mar 03 2025
+Last Modified: Mon Mar 03 2025
+Version: 1.0.0
+Description: 
+'''
+
+'''
+---------------------
+external libraries
+---------------------
+'''
 from astropy.io import fits
-import astropy.units as u
-from astropy.nddata import VarianceUncertainty
-from specutils import SpectrumCollection
-from specutils.utils.wcs_utils import gwcs_from_array
-import numpy as np
-from astropy.time import Time
-import pandas as pd
-import instruments.espresso.config.config as config
-# import base class
+import os
+
+
+'''
+---------------------
+internal libraries
+---------------------
+'''
 from core.models.level2 import RV2
-import instruments.espresso.utils as utils
+import instruments.espresso.config.config as config
+from instruments.espresso.utils import convert_S2D_BLAZE, convert_BLAZE, convert_DRIFT, get_files_names, create_PRIMARY, validate_fits_file
+
 # KPF Level2 Reader
 class ESPRESSORV2(RV2):
     """
-    Read a KPF level 1 file and convert it to the EPRV standard format Python object.
+    Read a HARPS level 1 file and convert it to the EPRV standard format Python object.
 
-    This class extends the `RV2` base class to handle the reading of KPF (Keck Planet Finder)
+    This class extends the `RV2` base class to handle the reading of HARPS (High Accuracy 
+    Radial velocity Planet Searcher)
     Level 1 files and converts them into a standardized EPRV
     format. Each extension from the FITS file is read, and relevant data, including flux,
     wavelength, variance, and metadata, are stored as attributes of the resulting Python object.
@@ -30,9 +45,9 @@ class ESPRESSORV2(RV2):
         data for different chips and fibers, and stores them in a standardized format.
 
         - The method processes science data (`SCI_FLUX`, `SCI_WAVE`, `SCI_VAR`) from both
-          the GREEN and RED chips and different fibers (`SKY`, `CAL`).
+            the GREEN and RED chips and different fibers (`SKY`, `CAL`).
         - For each chip and fiber, the flux, wavelength, variance, and metadata are extracted
-          and stored as a `SpectrumCollection` object.
+            and stored as a `SpectrumCollection` object.
         - Deletes unused extensions such as `RED_TELLURIC`, `GREEN_TELLURIC`, and `TELEMETRY`.
 
     Attributes
@@ -49,21 +64,71 @@ class ESPRESSORV2(RV2):
     Notes
     -----
     - The `_read` method processes science and calibration data from the GREEN and RED chips,
-      and it extracts and organizes data for both the SCI, SKY, and CAL fibers.
+        and it extracts and organizes data for both the SCI, SKY, and CAL fibers.
     - The method converts the flux, wavelength, and variance for each extension into
-      `SpectrumCollection` objects.
+        `SpectrumCollection` objects.
     - Unused extensions (like `RED_TELLURIC`, `GREEN_TELLURIC`, and `TELEMETRY`) are removed
-      from the object.
+        from the object.
 
     Example
     -------
     >>> from core.models.level2 import RV2
     >>> rv2_obj = RV2.from_fits("kpf_level1_file.fits")
     >>> rv2_obj.to_fits("standard_level2.fits")
-    """
+		"""
 
-    def _read(self, hdul: fits.HDUList) -> None:
-        #print(self.info())
-        utils.do_conversion(self, hdul)
-        return
+    def do_convertion(self, hdul: fits.HDUList) -> None:
+        """
+        Converts FITS files based on certain conditions and configurations.
+
+        This function performs the following tasks:
+        1. Validates the FITS file to ensure it meets the criteria for conversion.
+        2. Retrieves necessary file paths and names for further processing.
+        3. Converts S2D_BLAZE_A, S2D_BLAZE_B, BLAZE_A, and BLAZE_B files based on the given fibers.
+        4. Converts the Drift file.
+        5. Creates the PRIMARY header and adds necessary extensions.
+        6. Removes unnecessary extensions such as 'RECEIPT' and 'DRP_CONFIG'.
+
+        Args:
+            hdul (fits.HDUList): The FITS HDU list to be processed.
         
+        Raises:
+            ValueError: If the FITS file is invalid and does not meet the required criteria for conversion.
+        """
+
+        path = os.path.join(self.dirname, self.filename)
+
+        # Validate the FITS file before conversion. If it does not meet the criteria, raise an error
+        try :
+            validate_fits_file(path)
+            print("File is valid for conversion!")
+        except ValueError as e:
+            raise ValueError(e)
+
+        # Retrieve the paths for the necessary files
+        names = get_files_names(path)
+
+        # Convert S2D_BLAZE_A, S2D_BLAZE_B, BLAZE_A, and BLAZE_B files
+        trace_ind_start = 1
+
+        with fits.open(path) as hdu_raw:
+            dpr_type = hdu_raw['PRIMARY'].header['HIERARCH ESO DPR TYPE'].split(",")[1]
+        fibers = config.fiber.get(dpr_type, {})
+
+        for fiber in fibers:
+            convert_S2D_BLAZE(self, names["s2d_blaze_file_"+fiber], trace_ind_start, config.slice_nb)
+            convert_BLAZE(self, names["blaze_file_"+fiber], trace_ind_start, config.slice_nb)
+            trace_ind_start+=config.slice_nb
+
+        # Convert the Drift file
+        convert_DRIFT(self, names["drift_file_B"])
+
+        # Create the PRIMARY header
+        nb_fiber = len(fibers)
+        nb_trace = nb_fiber * config.slice_nb
+        create_PRIMARY(self, names, nb_trace, config.slice_nb)
+
+        # Remove unnecessary extensions
+        self.del_extension('RECEIPT')
+        self.del_extension('DRP_CONFIG')
+        print('end')
