59592d91e1d9bec45100b18e0cb0bf3b40d230d7
Danny Krolikowski <krolikowski@utexas.edu>
2025-03-18 12:16:27 -0700

    Update docstring

--- rvdata/instruments/neid/level2.py
+++ rvdata/instruments/neid/level2.py
@@ -13,7 +13,7 @@ from rvdata.core.models.level2 import RV2
 # NEID Level2 Reader
 class NEIDRV2(RV2):
     """
-    Read a NEID level 1 file and convert it to the EPRV standard format Python object.
+    Read a NEID Level 2 file and convert it to the EPRV L2 standard format Python object.
 
     This class extends the `RV2` base class to handle the reading of NEID Level 2 files and
     converts them into a standardized EPRV level 2 data format. Each extension from the FITS file
@@ -23,35 +23,35 @@ class NEIDRV2(RV2):
     Methods
     -------
     _read(hdul: fits.HDUList) -> None:
-        Reads the input FITS HDU list, extracts specific extensions related to the science
-        data for different chips and fibers, and stores them in a standardized format.
+        Reads the input FITS HDU list and extracts relevant information from specific extensions
+        related to the standardized format.
         - The method processes data from different fibers depending on NEID OBS-MODE:
-          SCI/SKY/CAL for HR mode and SCI/SKY for HE mode. (note about CAL extension in HE)
-        - For each fiber, the flux, wavelength, variance, and metadata are extracted and stored as
-          a `SpectrumCollection` object.
+          SCI/SKY/CAL for HR mode and SCI/SKY for HE mode (different NUMTRACE)
 
     Attributes
     ----------
     extensions : dict
-        A dictionary containing all the created extensions (e.g., `C1_SCI1`, `C1_SKY1`, `C1_CAL1`)
-        where the keys are the extension names and the values are `SpectrumCollection` objects
-        for each respective dataset.
+        A dictionary containing all the created extensions (e.g., `INSTRUMENT_HEADER`,
+        `TRACE1_FLUX`, `EXPMETER`) where the keys are the extension names and the values are the
+        data type (e.g., ImageHDU, BinTableHDU).
 
-    header : dict
+    headers : dict
         A dictionary containing metadata headers from the FITS file, with each extension's
         metadata stored under its respective key.
+
+    data : dict
+        A dictionary containing the data entries from the FITS file, with each extension's data
+        stored under its respective key.
 
     Notes
     -----
     - The `_read` method processes and organizes science and calibration data from all SCI, SKY,
       and CAL fibers.
-    - The method converts the flux, wavelength, and variance for each extension into
-      `SpectrumCollection` objects.
 
     Example
     -------
     >>> from astropy.io import fits
-    >>> hdul = fits.open('neidL1_YYYYMMDDTHHMMSS.fits')
+    >>> hdul = fits.open('neidL2_YYYYMMDDTHHMMSS.fits')
     >>> rv2_obj = NEIDRV2()
     >>> rv2_obj._read(hdul)
     """
