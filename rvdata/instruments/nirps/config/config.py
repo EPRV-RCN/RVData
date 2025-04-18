57609fd7f3b2fbf8f4c257dcc649d6f25a93c936
loris.jacques@yahoo.fr <loris.jacques@yahoo.fr>
2025-04-18 11:21:33 +0200

    Modif DRS_VERSION

--- rvdata/instruments/nirps/config/config.py
+++ rvdata/instruments/nirps/config/config.py
@@ -27,7 +27,7 @@ NUMORDER = 71
 data_format = "L2"  # Can either be original or L2
 
 # DRS version for proper file search functionality.
-DRS_VERSION = "DRS-3.2.5"
+DRS_VERSION = "DRS-3.2.0"
 
 # Allows the conversion of the RAW file
 extnames_raw = {
