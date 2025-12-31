cc23b15bba8578b7c32a6e9b6ee7dd8700aba452
Leonardo Paredes <49594379+phdparedes@users.noreply.github.com>
2025-12-31 14:05:58 -0700

    L3: Added Level 3 regression tests for NEID

--- rvdata/tests/regression/compliance.py
+++ rvdata/tests/regression/compliance.py
@@ -56,6 +56,58 @@ def check_l2_header(header):
             continue
 
 
+def check_l3_extensions(inpfile):
+    l3_csv = (
+        importlib.resources.files("rvdata.core.models.config") / "L3-extensions.csv"
+    )
+    reference_extensions = pd.read_csv(l3_csv)
+    extdf = reference_extensions
+    with fits.open(inpfile) as hdul:
+        # check that all required extensions exist
+        hdul = fits.open(inpfile)
+        for i, row in extdf.iterrows():
+            ext = row["Name"]
+            req = row["Required"]
+            if req:
+                assert ext in hdul, f"Required extension {ext} not found in {inpfile}"
+
+        # check that all extensions from the EXT_DESCRIPT table exist
+        ext_table = pd.DataFrame(hdul["EXT_DESCRIPT"].data)
+        for i, row in ext_table.iterrows():
+            extname = row["Name"]
+            assert (
+                extname in hdul
+            ), f"Extension {extname} not found in data but present in EXT_DESCRIPT table."
+
+        # Check every extension in the data (except PRIMARY) has an entry in EXT_DESCRIPT
+        ext_names_in_table = ext_table["Name"].tolist()
+        for hdu in hdul:
+            if hdu.name == "PRIMARY":
+                continue
+            assert (
+                hdu.name in ext_names_in_table
+            ), f"Extension {hdu.name} present in data but missing from EXT_DESCRIPT table."
+
+
+def check_l3_header(header):
+    ref_csv = (
+        importlib.resources.files("rvdata.core.models.config")
+        / "L3-PRIMARY-keywords.csv"
+    )
+    reference_header = pd.read_csv(ref_csv)
+    for i, row in reference_header.iterrows():
+        key = row["Keyword"].split()[0]
+        req = row["Required"]
+        if req and (key not in header):
+            print(key, req)
+            assert key in header, f"Keyword {key} not found in header"
+        elif req and (key in header):
+            value = header[key]
+            print(f"{key} = {value} ✓")
+        else:
+            continue
+
+
 def check_l4_extensions(inpfile):
     l4_csv = (
         importlib.resources.files("rvdata.core.models.config") / "L4-extensions.csv"
@@ -99,10 +151,9 @@ def check_l4_header(header):
         importlib.resources.files("rvdata.core.models.config")
         / "L4-PRIMARY-keywords.csv"
     )
-    reference_header = pd.concat([
-        pd.read_csv(ref_csv_l2),
-        pd.read_csv(ref_csv_l4)
-    ], ignore_index=True)
+    reference_header = pd.concat(
+        [pd.read_csv(ref_csv_l2), pd.read_csv(ref_csv_l4)], ignore_index=True
+    )
     for i, row in reference_header.iterrows():
         key = row["Keyword"]
         req = row["Required"] == "Y"
